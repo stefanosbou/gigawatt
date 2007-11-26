1037c103b3273473181899e406f77d1fc5b4c175
Amit.Chada <Amit.Chada@62ef49d1-3c3f-0410-a5d9-f1b620642450>
2007-11-26 02:58:11 +0000

    Fixed clone method implementation

--- src/com/dojoconsulting/oanda/fxtrade/api/FXTick.java
+++ src/com/dojoconsulting/oanda/fxtrade/api/FXTick.java
@@ -9,70 +9,70 @@ import java.util.Date;
  * An FXTick object represents a single forex spot price.
  */
 public final class FXTick implements Cloneable, ITick {
-    private long timestamp;
-    private double bid;
-    private double ask;
+	private long timestamp;
+	private double bid;
+	private double ask;
 
-    public FXTick() {
-        this.timestamp = 0;
-        this.bid = 0;
-        this.ask = 0;
-    }
+	public FXTick() {
+		this.timestamp = 0;
+		this.bid = 0;
+		this.ask = 0;
+	}
 
-    public FXTick(final long timestamp, final double bid, final double ask) {
-        this.timestamp = timestamp;
-        this.bid = bid;
-        this.ask = ask;
-    }
+	public FXTick(final long timestamp, final double bid, final double ask) {
+		this.timestamp = timestamp;
+		this.bid = bid;
+		this.ask = ask;
+	}
 
-    public long getTimestamp() {
-        return timestamp;
-    }
+	public long getTimestamp() {
+		return timestamp;
+	}
 
-    public double getBid() {
-        return bid;
-    }
+	public double getBid() {
+		return bid;
+	}
 
-    public double getAsk() {
-        return ask;
-    }
+	public double getAsk() {
+		return ask;
+	}
 
-    public FXTick getInverse() {
-        return new FXTick(timestamp, 1 / ask, 1 / bid);
-    }
+	public FXTick getInverse() {
+		return new FXTick(timestamp, 1 / ask, 1 / bid);
+	}
 
-    public double getMean() {
-        return (bid + ask) / 2;
-    }
+	public double getMean() {
+		return (bid + ask) / 2;
+	}
 
-    public void setTimestamp(final long timestamp) {
-        this.timestamp = timestamp;
-    }
+	public void setTimestamp(final long timestamp) {
+		this.timestamp = timestamp;
+	}
 
-    public void setBid(final double bid) {
-        this.bid = bid;
-    }
+	public void setBid(final double bid) {
+		this.bid = bid;
+	}
 
-    public void setAsk(final double ask) {
-        this.ask = ask;
-    }
+	public void setAsk(final double ask) {
+		this.ask = ask;
+	}
 
-    public boolean equals(final Object o) {
-        if (!(o instanceof FXTick)) {
-            return false;
-        }
-        final FXTick other = (FXTick) o;
-        return (this.timestamp == other.timestamp) && (this.bid == other.bid) && (this.ask == other.ask);
-    }
+	public boolean equals(final Object o) {
+		if (!(o instanceof FXTick)) {
+			return false;
+		}
+		final FXTick other = (FXTick) o;
+		return (this.timestamp == other.timestamp) && (this.bid == other.bid) && (this.ask == other.ask);
+	}
 
-    public Object clone() throws CloneNotSupportedException {
-        return super.clone();
-    }
+	public Object clone() {
+		return new FXTick(timestamp, bid, ask);
+	}
 
-    public String toString() {
-        final Date date = new Date(timestamp);
-        final String strDate = DateFormat.getDateInstance().format(date);
-        return strDate + ": " + timestamp + " " + bid + " " + ask;
-        //todoproper: Implement toString()
-    }
+	public String toString() {
+		final Date date = new Date(timestamp);
+		final String strDate = DateFormat.getDateInstance().format(date);
+		return strDate + ": " + timestamp + " " + bid + " " + ask;
+		//todoproper: Implement toString()
+	}
 }
