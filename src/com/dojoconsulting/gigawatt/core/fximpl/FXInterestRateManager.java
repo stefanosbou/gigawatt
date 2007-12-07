571479d4077b807e81da86988213349ce131a4c7
Nskaggs <Nskaggs@62ef49d1-3c3f-0410-a5d9-f1b620642450>
2007-12-07 19:37:51 +0000

    WIP - FXInterestRateManager.java updates

--- src/com/dojoconsulting/gigawatt/core/fximpl/FXInterestRateManager.java
+++ src/com/dojoconsulting/gigawatt/core/fximpl/FXInterestRateManager.java
@@ -4,9 +4,18 @@
 
 package com.dojoconsulting.gigawatt.core.fximpl;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.HashSet;
+
 import com.dojoconsulting.gigawatt.config.BackTestConfig;
 import com.dojoconsulting.gigawatt.core.IInterestRateManager;
+import com.dojoconsulting.gigawatt.core.fximpl.FXTradeManager.OrderMonitor;
 import com.dojoconsulting.oanda.fxtrade.api.Account;
+import com.dojoconsulting.oanda.fxtrade.api.FXPair;
 import com.dojoconsulting.oanda.fxtrade.api.MarketOrder;
 import com.dojoconsulting.oanda.fxtrade.api.Position;
 import com.dojoconsulting.oanda.fxtrade.api.Transaction;
@@ -20,6 +29,49 @@ public class FXInterestRateManager implements IInterestRateManager {
 	public void init(final BackTestConfig config) {
 		//TODO: Anything to init for int rate manager?
 		// Comment by AmitChada: You will need to get the interest rate file and want to create the db tables.
+//		long timestamp = 0;
+//		if (logger.isDebugEnabled()) {
+//			timestamp = System.currentTimeMillis();
+//			logger.debug("FXINterestRateManager: Started init()");
+//		}
+//		try {
+//			orderMonitors = new HashMap<String, OrderMonitor>();
+//			refreshPairs = new HashSet<FXPair>();
+//
+//			Class.forName("org.hsqldb.jdbcDriver");
+//			Connection tradeDB = DriverManager.getConnection("jdbc:hsqldb:mem:tradeDB", "sa", "");
+//			final Statement st = tradeDB.createStatement();
+//			//currency timeStartedInMillis bid ask
+//			final String expression = "CREATE TABLE rates ( currency INTEGER IDENTITY, accountId INTEGER, market CHAR(7), isLong BOOLEAN, stopLoss FLOAT, takeProfit FLOAT)";
+//			st.executeUpdate(expression);
+//			st.close();
+//
+//			tradeInsert = tradeDB.prepareStatement("INSERT INTO trades (tradeId, accountId, market, isLong, stopLoss, takeProfit) VALUES (?, ?, ?, ?, ?, ?)");
+//			tradeUpdate = tradeDB.prepareStatement("UPDATE trades SET stopLoss = ?, takeProfit = ? WHERE tradeId = ?");
+//			tradeSelectLongStopLoss = tradeDB.prepareStatement("SELECT max(stopLoss) FROM trades WHERE market = ? AND isLong = 1 AND stopLoss <> 0");
+//			tradeSelectLongTakeProfit = tradeDB.prepareStatement("SELECT min(takeProfit) FROM trades WHERE market = ? AND isLong = 1 and takeProfit <> 0");
+//			tradeSelectShortStopLoss = tradeDB.prepareStatement("SELECT min(stopLoss) FROM trades WHERE market = ? AND isLong = 0 AND stopLoss <> 0");
+//			tradeSelectShortTakeProfit = tradeDB.prepareStatement("SELECT max(takeProfit) FROM trades WHERE market = ? AND isLong = 0 and takeProfit <> 0");
+//			stopLossTradesLong = tradeDB.prepareStatement("SELECT tradeId, accountId FROM trades WHERE market = ? AND isLong = 1 AND stopLoss > ?");
+//			stopLossTradesShort = tradeDB.prepareStatement("SELECT tradeId, accountId FROM trades WHERE market = ? AND isLong = 0 AND stopLoss < ?");
+//
+//			takeProfitTradesLong = tradeDB.prepareStatement("SELECT tradeId, accountId FROM trades WHERE market = ? AND isLong = 1 AND takeProfit < ?");
+//			takeProfitTradesShort = tradeDB.prepareStatement("SELECT tradeId, accountId FROM trades WHERE market = ? AND isLong = 0 AND takeProfit > ?");
+//
+//			tradeClose = tradeDB.prepareStatement("DELETE FROM trades WHERE tradeId = ?");
+//			tradeCount = tradeDB.prepareStatement("SELECT count(*) FROM trades");
+//		}
+//		catch (SQLException e) {
+//			e.printStackTrace();  // Todoerror: Improve error handling
+//			System.exit(1);
+//		}
+//		catch (ClassNotFoundException e) {
+//			e.printStackTrace();  // Todoerror: Improve error handling
+//			System.exit(1);
+//		}
+//		if (logger.isDebugEnabled()) {
+//			logger.debug("FXInterestManager: Finished init() in " + (System.currentTimeMillis() - timestamp));
+//		}
 	}
 
 	public Transaction calcInterestForClosedTrade(final MarketOrder marketOrder) {
