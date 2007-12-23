773332bc646938add96ad577fd120a972622fa6d
Amit.Chada <Amit.Chada@62ef49d1-3c3f-0410-a5d9-f1b620642450>
2007-12-23 21:14:39 +0000

    Added boolean methods for isLong and isShort.  Promoted up from MarketOrder to be used by Limit/Entry orders.

--- trunk/src/com/dojoconsulting/oanda/fxtrade/api/Order.java
+++ trunk/src/com/dojoconsulting/oanda/fxtrade/api/Order.java
@@ -4,90 +4,98 @@ package com.dojoconsulting.oanda.fxtrade.api;
  * Note that a sell order is indicated by specifying negative units.
  */
 public abstract class Order {
-    private double highPriceLimit;
-    private double lowPriceLimit;
-    private FXPair pair;
-    private StopLossOrder stopLoss;
-    private TakeProfitOrder takeProfit;
-    private long units;
+	private double highPriceLimit;
+	private double lowPriceLimit;
+	private FXPair pair;
+	private StopLossOrder stopLoss;
+	private TakeProfitOrder takeProfit;
+	private long units;
 
-    private long timestamp;
-    private int transactionNumber;
+	private long timestamp;
+	private int transactionNumber;
 
-    private double price;
+	private double price;
 
-    public Order() {
-    }
+	public Order() {
+	}
 
-    void setTimestamp(final long timestamp) {
-        this.timestamp = timestamp;
-    }
+	void setTimestamp(final long timestamp) {
+		this.timestamp = timestamp;
+	}
 
-    void setTransactionNumber(final int transactionNumber) {
-        this.transactionNumber = transactionNumber;
-    }
+	void setTransactionNumber(final int transactionNumber) {
+		this.transactionNumber = transactionNumber;
+	}
 
-    void setPrice(final double price) {
-        this.price = price;
-    }
+	void setPrice(final double price) {
+		this.price = price;
+	}
 
-    public long getTimestamp() {
-        return timestamp;
-    }
+	public long getTimestamp() {
+		return timestamp;
+	}
 
-    public int getTransactionNumber() {
-        return transactionNumber;
-    }
+	public int getTransactionNumber() {
+		return transactionNumber;
+	}
 
-    public double getPrice() {
-        return price;
-    }
+	public double getPrice() {
+		return price;
+	}
 
-    public double getHighPriceLimit() {
-        return highPriceLimit;
-    }
+	public double getHighPriceLimit() {
+		return highPriceLimit;
+	}
 
-    public void setHighPriceLimit(final double highPriceLimit) {
-        this.highPriceLimit = highPriceLimit;
-    }
+	public void setHighPriceLimit(final double highPriceLimit) {
+		this.highPriceLimit = highPriceLimit;
+	}
 
-    public double getLowPriceLimit() {
-        return lowPriceLimit;
-    }
+	public double getLowPriceLimit() {
+		return lowPriceLimit;
+	}
 
-    public void setLowPriceLimit(final double lowPriceLimit) {
-        this.lowPriceLimit = lowPriceLimit;
-    }
+	public void setLowPriceLimit(final double lowPriceLimit) {
+		this.lowPriceLimit = lowPriceLimit;
+	}
 
-    public FXPair getPair() {
-        return pair;
-    }
+	public FXPair getPair() {
+		return pair;
+	}
 
-    public void setPair(final FXPair pair) {
-        this.pair = pair;
-    }
+	public void setPair(final FXPair pair) {
+		this.pair = pair;
+	}
 
-    public StopLossOrder getStopLoss() {
-        return stopLoss;
-    }
+	public StopLossOrder getStopLoss() {
+		return stopLoss;
+	}
 
-    public void setStopLoss(final StopLossOrder stopLoss) {
-        this.stopLoss = stopLoss;
-    }
+	public void setStopLoss(final StopLossOrder stopLoss) {
+		this.stopLoss = stopLoss;
+	}
 
-    public TakeProfitOrder getTakeProfit() {
-        return takeProfit;
-    }
+	public TakeProfitOrder getTakeProfit() {
+		return takeProfit;
+	}
 
-    public void setTakeProfit(final TakeProfitOrder takeProfit) {
-        this.takeProfit = takeProfit;
-    }
+	public void setTakeProfit(final TakeProfitOrder takeProfit) {
+		this.takeProfit = takeProfit;
+	}
 
-    public long getUnits() {
-        return units;
-    }
+	public long getUnits() {
+		return units;
+	}
 
-    public void setUnits(final long units) {
-        this.units = units;
-    }
+	public void setUnits(final long units) {
+		this.units = units;
+	}
+
+	boolean isLong() {
+		return getUnits() > 0;
+	}
+
+	boolean isShort() {
+		return getUnits() < 0;
+	}
 }
