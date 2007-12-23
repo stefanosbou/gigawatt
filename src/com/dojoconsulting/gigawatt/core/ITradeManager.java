2e4960d5f907b3f6a3f6f6a3bc1e1fef86eb359c
Amit.Chada <Amit.Chada@62ef49d1-3c3f-0410-a5d9-f1b620642450>
2007-12-23 21:35:36 +0000

    Moved method declarations into the interfaces when needed by other components.  This is to prevent the need for casting to implementation instance.

--- src/com/dojoconsulting/gigawatt/core/ITradeManager.java
+++ src/com/dojoconsulting/gigawatt/core/ITradeManager.java
@@ -1,12 +1,20 @@
 package com.dojoconsulting.gigawatt.core;
 
+import com.dojoconsulting.gigawatt.core.fximpl.domain.TransactionType;
+import com.dojoconsulting.oanda.fxtrade.api.Account;
+import com.dojoconsulting.oanda.fxtrade.api.MarketOrder;
+
 /**
  * Created by IntelliJ IDEA.
  * User: Amit Chada
  * Date: 15-Oct-2007
  * Time: 05:23:12
- * To change this template use File | Settings | File Templates.
  */
 public interface ITradeManager extends IEngineProcess {
 
+	void closeTrade(MarketOrder closeTrade, TransactionType transactionType);
+
+	void modifyTrade(MarketOrder mo);
+
+	void executeTrade(MarketOrder mo, Account account, TransactionType transactionType);
 }
