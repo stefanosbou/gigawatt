b9df044d40a57534cadf0a4b874ad5697f7ea0fa
Amit.Chada <Amit.Chada@62ef49d1-3c3f-0410-a5d9-f1b620642450>
2007-12-27 07:22:41 +0000

    Added datasource to transaction manager.

--- trunk/src/com/dojoconsulting/gigawatt/core/fximpl/FXTransactionManager.java
+++ trunk/src/com/dojoconsulting/gigawatt/core/fximpl/FXTransactionManager.java
@@ -2,6 +2,13 @@ package com.dojoconsulting.gigawatt.core.fximpl;
 
 import com.dojoconsulting.gigawatt.config.BackTestConfig;
 import com.dojoconsulting.gigawatt.core.ITransactionManager;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by IntelliJ IDEA.
@@ -11,9 +18,34 @@ import com.dojoconsulting.gigawatt.core.ITransactionManager;
  */
 public class FXTransactionManager implements ITransactionManager {
 
+	private static Log logger = LogFactory.getLog(FXTransactionManager.class);
+
 	private int nextTicketNumber = 1;
+	private DataSource dataSource;
 
 	public void init(final BackTestConfig config) {
+		long timestamp = 0;
+		if (logger.isDebugEnabled()) {
+			timestamp = System.currentTimeMillis();
+			logger.debug("FXTransactionManager: Started init()");
+		}
+		try {
+			final Connection orderDB = dataSource.getConnection();
+
+			final Statement st = orderDB.createStatement();
+			st.executeUpdate("DROP TABLE transactions IF EXISTS");
+			final String expression = "CREATE TABLE transactions ( transactionNumber INTEGER, accountId INTEGER, market CHAR(7), isLong BOOLEAN, isAbovePrice BOOLEAN, price FLOAT, stopLoss FLOAT, takeProfit FLOAT, expiry BIGINT)";
+			st.executeUpdate(expression);
+			st.close();
+
+		}
+		catch (SQLException e) {
+			e.printStackTrace();  // Todoerror: Improve error handling
+			System.exit(1);
+		}
+		if (logger.isDebugEnabled()) {
+			logger.debug("FXTransactionManager: Finished init() in " + (System.currentTimeMillis() - timestamp));
+		}
 	}
 
 	public void close() {
@@ -21,5 +53,9 @@ public class FXTransactionManager implements ITransactionManager {
 
 	public int getNextTransactionNumber() {
 		return nextTicketNumber++;
+	}
+
+	public void setDataSource(final DataSource dataSource) {
+		this.dataSource = dataSource;
 	}
 }
